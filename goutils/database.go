b467c07c0da03f99afb402d444b121f8e9e6f304
FDSAP - Raymond <raymond.bago@fortress-asya.com>
2023-01-23 00:05:00 +0800

    first commit

---
+++ goutils/database.go
@@ -0,0 +1,25 @@
+package goutils
+
+import (
+	"fmt"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var (
+	DBConnect *gorm.DB
+	DBErr     error
+)
+
+func ConnectDB(host, user, pass, dbname, port string) {
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s, sslmode=disable TimeZone=Asia/Shanghai", host, user, pass, dbname, port)
+	DBConnect, DBErr = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+}
+
+func MigrateModel(model interface{}) {
+	migratErr := DBConnect.AutoMigrate(model)
+	if migratErr != nil {
+		fmt.Println("MIGRATE ERR:", migratErr)
+	}
+}
---
+++ main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"golang_training/goutils"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	//API of CREATE READ UPDATE DELETE
+	//Connect to postgreSQL
+	goutils.ConnectDB("localhost", "postgres", "1Qaz2wsx", "postgres", "5432")
+	//Create table
+	goutils.MigrateModel(UserModel{})
+
+	app := fiber.New()
+
+	//CREATE
+	app.Post("/save", func(c *fiber.Ctx) error {
+		requestModel := &UserModel{}
+		c.BodyParser(requestModel)
+
+		goutils.DBConnect.Debug().Raw("INSERT INTO user_models(name) VALUES(?)", requestModel.Name).Scan(requestModel)
+		return c.JSON(requestModel)
+	})
+	//READ
+	app.Get("/view", func(c *fiber.Ctx) error {
+		responseModel := &[]UserModel{}
+
+		goutils.DBConnect.Debug().Raw("SELECT * FROM user_models").Find(responseModel)
+
+		return c.JSON(responseModel)
+	})
+	//UPDATE
+	app.Post("/update/:name?", func(c *fiber.Ctx) error {
+		var1 := c.Params("name")
+		requestModel := &UserModel{}
+		c.BodyParser(requestModel)
+
+		goutils.DBConnect.Debug().Raw("UPDATE user_models SET name=? WHERE name=?", requestModel.Name, var1).Scan(requestModel)
+
+		return c.JSON(fiber.Map{
+			"Result": requestModel,
+		})
+	})
+	//DELETE
+	app.Delete("/delete/:name", func(c *fiber.Ctx) error {
+		var1 := c.Params("name")
+		responseModel := &UserModel{}
+
+		if deletErr := goutils.DBConnect.Debug().Raw("DELETE FROM user_models WHERE name=?", var1).Scan(responseModel).Error; deletErr != nil {
+			c.JSON(fiber.Map{
+				"Error": deletErr,
+			})
+		}
+
+		return c.JSON(fiber.Map{
+			"Result": "successfully deleted",
+		})
+
+	})
+	app.Listen(":3000")
+}
+
+// Model
+type UserModel struct {
+	Id   uint   `json:"-" gorm:"primaryKey"`
+	Name string `json:"name"`
+}
